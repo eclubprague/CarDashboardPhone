d40bd4b77a09b29fbcda7414a616443fc6942ab4
Lukas Hruby <lukas.hruby@gmail.com>
2015-07-15 16:05:47 +0200

    VerticalViewPager implemented

---
+++ app/src/main/java/com/eclubprague/cardashboard/phone/ScreenSlidePageFragment.java
@@ -0,0 +1,27 @@
+package com.eclubprague.cardashboard.phone;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ScreenSlidePageFragment extends Fragment {
+    private static final int NUM_PAGES = 5;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+        return rootView;
+    }
+
+
+}
